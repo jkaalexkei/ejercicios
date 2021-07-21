0b057bd0791a7c1bdfde530a96c171bc3a2c4de7
alex <jkaalexkei@gmail.com>
2021-07-21 16:13:27 -0500

    trabajando practicas con decoradores2

--- prueba.py
+++ prueba.py
@@ -75,3 +75,23 @@ def saludar():
     
 
 
+def measure_time(function):
+    def wrapper(*args, **kwargs):
+        import time
+
+        start = time.time()
+        result = function(*args, **kwargs)
+        total = time.time() - start
+        print(total, 'seconds' )
+        return result
+
+    return wrapper
+
+
+@measure_time
+def suma(a, b):
+    import time
+    time.sleep(1)
+    return a + b
+
+print(suma(10, 20))
