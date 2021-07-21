797ec458bcac6f87015e66cbb6576254933bda8c
alex <jkaalexkei@gmail.com>
2021-07-21 15:06:56 -0500

    funcion palindrome

--- prueba.py
+++ prueba.py
@@ -1,21 +1,36 @@
 
 #decoradores
 
-def fizzbuzz(numero):
+# def fizzbuzz(numero):
     
-    if (numero % 3 == 0) and (numero % 5 == 0):
-        return 'fizzbuzz'
+#     if (numero % 3 == 0) and (numero % 5 == 0):
+#         return 'fizzbuzz'
     
-    elif numero % 5==0:
-        return 'fizz'
-    elif numero % 3 == 0:
-        return 'buzz'
-    else:
-        return numero
+#     elif numero % 5==0:
+#         return 'fizz'
+#     elif numero % 3 == 0:
+#         return 'buzz'
+#     else:
+#         return numero
         
 
-lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+# lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
-for i in lista:
-    print(fizzbuzz(i))
+# for i in lista:
+#     print(fizzbuzz(i))
 
+
+def palindromo(texto):
+    
+    lista = []
+    
+    for elementos in texto:
+        lista.append(elementos)
+        
+    textocontrario = lista[::-1]
+    
+    comparacion = 'palindromo' if lista == textocontrario else 'no es palindromo'
+    
+    return comparacion
+
+print(palindromo('ana'))
